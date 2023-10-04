113241e5da012f582f0de0d1286713f51d7ec29f
Zoonekynd Alexandre <alexzoonekynd@gmail.com>
2023-10-04 22:24:08 +0200

    add js file

---
+++ Javascript/Dog - Human years/app.js
@@ -0,0 +1,23 @@
+const myAge = 20
+
+// my real aage as human being
+
+let earlyYears = 2
+
+// Two first years of a dog life
+
+earlyYears *= 10.5
+
+let laterYears = myAge - 2
+
+laterYears *= 4
+
+// console.log(earlyYears, laterYears)
+
+let myAgeInDogYears = earlyYears + laterYears
+
+const myName = "Alex".toLowerCase()
+
+// Result
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years`)
