d1f8d233581a236456e953dbd4ff6d4c9eb6367d
Zoonekynd Alexandre <alexzoonekynd@gmail.com>
2023-11-26 10:11:21 +0100

    add file

---
+++ Javascript/message_generator/main.js
@@ -0,0 +1,18 @@
+// generateur de message aléatoire
+
+// 1 - crée une collection de message, avec des catégories
+const messages = {
+  greetings: ["Bonjour", "Salut", "Bye"],
+  motivation: [
+    "La motivation est la clé qui transforme les rêves en réalité, allumez-la et ouvrez la porte de votre succès.",
+    "Dans le livre de la vie, la motivation est l'encre qui donne vie à vos chapitres les plus audacieux.",
+    "L'énergie de la motivation est le carburant qui propulse chaque petit pas vers le sommet de vos aspirations.",
+  ],
+  salutation: [
+    "Namaste, que la lumière en moi salue la lumière en toi.",
+    "Om Shanti, que la paix soit avec toi.",
+    "Sukhi hotu, que tu sois heureux et en paix.",
+  ],
+};
+
+// 2 - Sélectionner dans ces messages un message et l'afficher à l'utilisateur
