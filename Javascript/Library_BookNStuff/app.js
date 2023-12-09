57447f17b8af33d0eed71295531fe2cc37bde890
Zoonekynd Alexandre <alexzoonekynd@gmail.com>
2023-12-09 18:00:41 +0100

    init projet library

---
+++ Javascript/Library_BookNStuff/app.js
@@ -0,0 +1,14 @@
+// create the four classes without
+
+// Book
+// Properties: author (string), title (string), pages (number), isCheckedOut (boolean, initially false), and ratings (array, initially empty).
+// Getters: all properties have a getter
+// Methods: .getAverageRating(), .toggleCheckOutStatus(), and .addRating()
+
+class Media {
+  constructor(title, ratings) {
+    this._title = title;
+    this._ratings = ratings;
+    isCheckedOut = false;
+  }
+}
